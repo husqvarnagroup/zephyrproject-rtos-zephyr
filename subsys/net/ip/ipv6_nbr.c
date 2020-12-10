7c97d2090d151517093775de6c363946d593d339
Jan Georgi <jan.georgi@lemonbeat.com>
2020-12-10 14:12:10 +0100

    [lb noup] net: ipv6: nbr: Disable on LB ifaces
    On Lemonbeat interfaces, we use the compressed IPv6 address to create a
    link-local address from. Therefore we skip the neighbor discovery here,
    so the packet will be sent to the link-local address specified inside
    the IPv6 destination address.
    
    Issue: LBEOS-706
    
    (cherry picked from commit 866b1bb29b3036fc0197f21c0b6978da1d8ba589)

--- subsys/net/ip/ipv6_nbr.c
+++ subsys/net/ip/ipv6_nbr.c
@@ -912,6 +912,15 @@ ignore_frag_error:
 	}
 
 try_send:
+#if defined(CONFIG_NET_L2_LEMONBEAT)
+	if (net_if_l2(iface) == &NET_L2_GET_NAME(LEMONBEAT)) {
+		/* On lb_radio interfaces, we use the compressed IPv6 address to create a link-local
+		 * address from. Therefore we skip the neighbor discovery here, so the packet will
+		 * be sent to the link-local address specified inside the IPv6 destination address.
+		 */
+		return NET_OK;
+	}
+#endif
 	net_ipv6_nbr_lock();
 
 	nbr = nbr_lookup(&net_neighbor.table, iface, nexthop);
