husqvarnagroup/zephyr

[lb fromlist] factory data: Add new subsystem
Factory data are device specific data like serial number, MAC addresses,
calibration data, and more. Factory data are expected to never be
changed (not even extended) during the whole (post-manufacturing)
lifetime of a device.

At first glance, the existing setting system seems like a solution to
this problem. However, (IoT) devices might use the settings module for
actual user settings. User settings data gets changed regularly,
contains personal and other relevant information like network keys and
therefore must be deleted (securely!) when a factory reset gets issued.

Non-volatile storage (NVS), the recommended settings backend, does not
provide any functionality to deterministically, physically erase stored
data. Physical erasure of user data can only be achieved by deleting the
flash pages of the underlying NVS storage. Therefore, the same NVS
storage can not be used for factory data and user settings.

Moreover, while it is possible to register multiple source backends to
the settings module, there can be only one write backend at any time.
Also, support for multiple backends seems to be regarded as an anti
feature (see PR #19798), not worth to be pushed any further.

For those reasons, using the settings module is not an option.

This commit adds a new module named factory_data. Similar to the
settings module it allows to read and list entries. It will use a
different partition however and writing data is only possible when
CONFIG_FACTORY_DATA_WRITE is enabled. Except for manufacturing
firmwares, this should never be the case.

Unlike the settings module, this module uses the flash circular buffer
(FCB) storage per default, as NVS would waste one flash page for a
functionality this modules will never need: power-cut save writes

A second, length-value based backend is available for devices with
limiting code size constraints, but reduced requirements regarding data
integrity.

A third, dummy backend is available t

include/zephyr/factory_data/factory_data.h
@@ -1,0 +1,114 @@
+/*
+ * Copyright (c) 2022 GARDENA GmbH
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_FACTORY_DATA_FACTORY_DATA_H_
+#define ZEPHYR_INCLUDE_FACTORY_DATA_FACTORY_DATA_H_
+
+#include <sys/types.h>
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define FACTORY_DATA_TOTAL_LEN_MAX                                                                 \
+	(CONFIG_FACTORY_DATA_NAME_LEN_MAX + 1 /* '\0' */ + CONFIG_FACTORY_DATA_VALUE_LEN_MAX)
+
+/* Relevant only when backed by flash */
+#define FACTORY_DATA_FLASH_PARTITION FLASH_AREA_ID(factory)
+
+/**
+ * @brief Public API of the factory data module.
+ *
+ * Accessing factory data.
+ *
+ * @defgroup factory_data Factory data
+ * @{
+ */
+
+/**
+ * Initialization of factory data subsystem
+ *
+ * Can be called at application startup.
+ *
+ * @return 0 on success, non-zero on failure.
+ */
+int factory_data_init(void);
+
+#if CONFIG_FACTORY_DATA_WRITE
+/**
+ * Write a single serialized value to factory data storage.
+ *
+ * @note Overwriting an already existing value (same name) is not possible.
+ *
+ * @param name Name/key of the factory data item.
+ * @param value Pointer to the value of the factory data item.
+ * @param val_len Length of the value.
+ *
+ * @return 0 on success, non-zero on failure.
+ */
+int factory_data_save_one(const char *name, const void *value, size_t val_len);
+
+/**
+ * Clear all factory data
+ *
+ * @return 0 on success; non-zero on failure
+ */
+int factory_data_erase(void);
+#endif /* CONFIG_FACTORY_DATA_WRITE */
+
+/**
+ * Callback function used for loading (all) factory data.
+ *
+ * Used by @ref factory_data_load function.
+ *
+ * @param[in]     name    Name of the factory data element
+ * @param[in]     value   Factory data read from flash
+ * @param[in]     len     Size of the factory data value in bytes
+ * @param[in,out] param   parameter given to the @ref factory_data_load function.
+ *
+ * @return 0 continue loading, non-zero stop loading data.
+ */
+typedef int (*factory_data_load_direct_cb)(const char *name, const uint8_t *value, size_t len,
+					   const void *param);
+
+/**
+ * Load factory data using given callback.
+ *
+ * @param[in]     cb      pointer to the callback function.
+ * @param[in,out] param   parameter to be passed when callback
+ *                        function is called.
+ * @retval 0 on success, all elements walked
+ * @retval negative on failure; entries up to the faulty one walked
+ * @retval positive when aborted by callback; entries up to the aborting one walked
+ */
+int factory_data_load(factory_data_load_direct_cb cb, const void *param);
+
+/**
+ * Load single factory data value.
+ *
+ * @note Calling this function causes (worst case) all existing elements to be walked and read out.
+ *       Whenever possible, use @ref factory_data_load instead.
+ *
+ * @param name Name/key of the factory data item.
+ * @param value Pointer to the value of the factory data item.
+ * @param val_len Length of the value.
+ *
+ * @return Number of read bytes, negative on failure.
+ */
+ssize_t factory_data_load_one(const char *name, uint8_t *value, size_t val_len);
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_INCLUDE_FACTORY_DATA_FACTORY_DATA_H_ */

husqvarnagroup/zephyrproject-rtos-zephyr

(cherry picked from commit f9965595f8fbbf44987810a38e34b597dbdcdc0d)

include/zephyr/factory_data/factory_data.h
@@ -1,0 +1,114 @@
+/*
+ * Copyright (c) 2022 GARDENA GmbH
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_FACTORY_DATA_FACTORY_DATA_H_
+#define ZEPHYR_INCLUDE_FACTORY_DATA_FACTORY_DATA_H_
+
+#include <sys/types.h>
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define FACTORY_DATA_TOTAL_LEN_MAX                                                                 \
+	(CONFIG_FACTORY_DATA_NAME_LEN_MAX + 1 /* '\0' */ + CONFIG_FACTORY_DATA_VALUE_LEN_MAX)
+
+/* Relevant only when backed by flash */
+#define FACTORY_DATA_FLASH_PARTITION FIXED_PARTITION_ID(factory_partition)
+
+/**
+ * @brief Public API of the factory data module.
+ *
+ * Accessing factory data.
+ *
+ * @defgroup factory_data Factory data
+ * @{
+ */
+
+/**
+ * Initialization of factory data subsystem
+ *
+ * Can be called at application startup.
+ *
+ * @return 0 on success, non-zero on failure.
+ */
+int factory_data_init(void);
+
+#if CONFIG_FACTORY_DATA_WRITE
+/**
+ * Write a single serialized value to factory data storage.
+ *
+ * @note Overwriting an already existing value (same name) is not possible.
+ *
+ * @param name Name/key of the factory data item.
+ * @param value Pointer to the value of the factory data item.
+ * @param val_len Length of the value.
+ *
+ * @return 0 on success, non-zero on failure.
+ */
+int factory_data_save_one(const char *name, const void *value, size_t val_len);
+
+/**
+ * Clear all factory data
+ *
+ * @return 0 on success; non-zero on failure
+ */
+int factory_data_erase(void);
+#endif /* CONFIG_FACTORY_DATA_WRITE */
+
+/**
+ * Callback function used for loading (all) factory data.
+ *
+ * Used by @ref factory_data_load function.
+ *
+ * @param[in]     name    Name of the factory data element
+ * @param[in]     value   Factory data read from flash
+ * @param[in]     len     Size of the factory data value in bytes
+ * @param[in,out] param   parameter given to the @ref factory_data_load function.
+ *
+ * @return 0 continue loading, non-zero stop loading data.
+ */
+typedef int (*factory_data_load_direct_cb)(const char *name, const uint8_t *value, size_t len,
+					   const void *param);
+
+/**
+ * Load factory data using given callback.
+ *
+ * @param[in]     cb      pointer to the callback function.
+ * @param[in,out] param   parameter to be passed when callback
+ *                        function is called.
+ * @retval 0 on success, all elements walked
+ * @retval negative on failure; entries up to the faulty one walked
+ * @retval positive when aborted by callback; entries up to the aborting one walked
+ */
+int factory_data_load(factory_data_load_direct_cb cb, const void *param);
+
+/**
+ * Load single factory data value.
+ *
+ * @note Calling this function causes (worst case) all existing elements to be walked and read out.
+ *       Whenever possible, use @ref factory_data_load instead.
+ *
+ * @param name Name/key of the factory data item.
+ * @param value Pointer to the value of the factory data item.
+ * @param val_len Length of the value.
+ *
+ * @return Number of read bytes, negative on failure.
+ */
+ssize_t factory_data_load_one(const char *name, uint8_t *value, size_t val_len);
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_INCLUDE_FACTORY_DATA_FACTORY_DATA_H_ */