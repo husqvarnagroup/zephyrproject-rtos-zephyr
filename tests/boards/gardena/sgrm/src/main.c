511b941212e38dbdab321b3f7495a8bd9ad71297
Reto Schneider <reto.schneider@husqvarnagroup.com>
2025-09-18 00:48:49 +0200

    tests: boards: gardena: Test communication with Si4467
    The GARDENA smart garden gateway features a Silicon Labs Si4467 sub-GHz
    transceiver. The TRX is attached to the smart garden radio module (SGRM)
    via SPI (and GPIOs). This new test implicitly ensures that the SPI
    driver of the SiM3U works as expected.

---
+++ tests/boards/gardena/sgrm/src/main.c
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2025 GARDENA GmbH
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/ztest.h>
+
+ZTEST_SUITE(spi_si4467, NULL, NULL, NULL, NULL, NULL);
