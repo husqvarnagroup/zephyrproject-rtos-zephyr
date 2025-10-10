husqvarnagroup/zephyr

[sg noup] net: lwm2m: Automatic send mechanism
Documentation for this feature can be found in our sg-zephyr-firmware
repo.

It seems like nobody besides us uses LwM2M send, otherwise we would not
have to build something on our own.

This commit noup because upstreaming it would require substantial
software design and discussion work upstream.

The command to sync all IPSOs is intended as a fall-back solution for
any kind of issues in the field.

Issue: R2D2-10191
Issue: R2D2-10635
Issue: R2D2-10946
Issue: R2D2-11646

(cherry picked from commit 33a3a7fc54737661040d17168541619b41dc7251)
(squashed a8dbe5a61843a0c11399096a0aab921dd34ba659)
(squashed 7c17585076f5849fc378e4253e955b3638da364c)
(squashed e1dfc3fd53181c73e95700acc9529a0e523ba76d)
(squashed 15b9a06d6921468a1cc67ad693ffacecfd083b97)
(squashed 1d4f81c3a5bc1a76a9a8504c61327d34995e4620)
(squashed 4d0c6cff1798ad51cd7c2a6890f4a212692352f1)
(squashed 3615e41077d437ce8a4b9e288a675ee1f46dee19)
(squashed eff567d4ce5fcc494d9405c7364c67bf9af0f15d)
(squashed f7176c9ae257810cd4d4837c0496fe2f6206d52b)
(squashed 86ec7f6ea68669cd9b9525f8d373e03a1c80b7b2)

subsys/net/lib/lwm2m/lwm2m_obj_security.c
@@ -147,6 +147,12 @@ static struct lwm2m_engine_obj_inst *security_create(uint16_t obj_inst_id)
 		     sizeof(res[index][0]) * ARRAY_SIZE(res[index]));
 	init_res_instance(res_inst[index], ARRAY_SIZE(res_inst[index]));
 
+#if defined(CONFIG_LWM2M_ENGINE_AUTO_SEND)
+	for (size_t res_index = 0; res_index < ARRAY_SIZE(res_inst[index]); res_index++) {
+		res_inst[index][res_index].ignore = true;
+	}
+#endif
+
 	/* initialize instance resource data */
 	INIT_OBJ_RES_DATA_LEN(SECURITY_SERVER_URI_ID, res[index], i,
 			  res_inst[index], j,

subsys/net/lib/lwm2m/lwm2m_object.h
@@ -391,6 +391,13 @@ struct lwm2m_engine_res_inst {
 	uint16_t data_len;
 	uint16_t res_inst_id; /* 65535 == not "created" */
 	uint8_t  data_flags;
+#if defined(CONFIG_LWM2M_ENGINE_AUTO_SEND)
+	uint8_t dirty: 1;
+	uint8_t force: 1;
+	uint8_t sending: 1;
+	uint8_t ignore: 1;
+	uint8_t report_after_write: 1;
+#endif
 };
 
 struct lwm2m_engine_res {
@@ -429,6 +436,13 @@ static inline void init_res_instance(struct lwm2m_engine_res_inst *ri,
 	memset(ri, 0, sizeof(*ri) * ri_len);
 	for (i = 0; i < ri_len; i++) {
 		ri[i].res_inst_id = RES_INSTANCE_NOT_CREATED;
+#if defined(CONFIG_LWM2M_ENGINE_AUTO_SEND)
+		ri[i].dirty = false;
+		ri[i].force = IS_ENABLED(CONFIG_LWM2M_ENGINE_AUTO_SEND_FORCE_SEND);
+		ri[i].sending = false;
+		ri[i].ignore = false;
+		ri[i].report_after_write = true;
+#endif
 	}
 }
 

subsys/net/lib/lwm2m/lwm2m_registry.c
@@ -519,10 +519,75 @@ int lwm2m_set_res_buf(const struct lwm2m_obj_path *path, void *buffer_ptr, uint1
 	res_inst->data_len = data_len;
 	res_inst->max_data_len = buffer_len;
 	res_inst->data_flags = data_flags;
+#if defined(CONFIG_LWM2M_ENGINE_AUTO_SEND)
+	res_inst->dirty = true;
+#endif
 
 	k_mutex_unlock(&registry_lock);
 	return ret;
 }
+
+#if defined(CONFIG_LWM2M_ENGINE_AUTO_SEND)
+int lwm2m_set_res_force_send(const struct lwm2m_obj_path *path, bool force)
+{
+	int ret;
+	struct lwm2m_engine_res_inst *res_inst = NULL;
+
+	if (path->level < LWM2M_PATH_LEVEL_RESOURCE) {
+		LOG_ERR("path must have at least 3 parts");
+		return -EINVAL;
+	}
+
+	k_mutex_lock(&registry_lock, K_FOREVER);
+	/* look up resource obj */
+	ret = path_to_objs(path, NULL, NULL, NULL, &res_inst);
+	if (ret < 0) {
+		k_mutex_unlock(&registry_lock);
+		return ret;
+	}
+
+	if (!res_inst) {
+		LOG_ERR("res instance %d not found", path->res_inst_id);
+		k_mutex_unlock(&registry_lock);
+		return -ENOENT;
+	}
+
+	res_inst->force = force;
+
+	k_mutex_unlock(&registry_lock);
+	return ret;
+}
+
+int lwm2m_set_res_report_after_write(const struct lwm2m_obj_path *path, bool report_after_write)
+{
+	int ret;
+	struct lwm2m_engine_res_inst *res_inst = NULL;
+
+	if (path->level < LWM2M_PATH_LEVEL_RESOURCE) {
+		LOG_ERR("path must have at least 3 parts");
+		return -EINVAL;
+	}
+
+	k_mutex_lock(&registry_lock, K_FOREVER);
+	/* look up resource obj */
+	ret = path_to_objs(path, NULL, NULL, NULL, &res_inst);
+	if (ret < 0) {
+		k_mutex_unlock(&registry_lock);
+		return ret;
+	}
+
+	if (!res_inst) {
+		LOG_ERR("res instance %d not found", path->res_inst_id);
+		k_mutex_unlock(&registry_lock);
+		return -ENOENT;
+	}
+
+	res_inst->report_after_write = report_after_write;
+
+	k_mutex_unlock(&registry_lock);
+	return ret;
+}
+#endif
 
 int lwm2m_engine_set_res_buf(const char *pathstr, void *buffer_ptr, uint16_t buffer_len,
 			     uint16_t data_len, uint8_t data_flags)
@@ -776,6 +841,11 @@ static int lwm2m_engine_set(const struct lwm2m_obj_path *path, const void *value
 		ret = res->post_write_cb(obj_inst->obj_inst_id, res->res_id, res_inst->res_inst_id,
 					 data_ptr, len, false, 0);
 	}
+
+#if defined(CONFIG_LWM2M_ENGINE_AUTO_SEND)
+	changed |= res_inst->force;
+	res_inst->dirty |= changed;
+#endif
 
 	if (changed && LWM2M_HAS_PERM(obj_field, LWM2M_PERM_R)) {
 		lwm2m_notify_observer_path(path);

husqvarnagroup/zephyrproject-rtos-zephyr

Auto-send is easier to maintain if we move the implementation to
`sg-zephyr-firmware` and keep only the nesseccary function calls and
modifications in the zephyr fork. Keeping it in-tree would make upstreaming
easier, but this is not planned any time soon.
(cherry picked from commit 0d75ae162b141864b9e427e49533ace873e855da)

subsys/net/lib/lwm2m/lwm2m_obj_security.c
@@ -147,6 +147,12 @@ static struct lwm2m_engine_obj_inst *security_create(uint16_t obj_inst_id)
 		     sizeof(res[index][0]) * ARRAY_SIZE(res[index]));
 	init_res_instance(res_inst[index], ARRAY_SIZE(res_inst[index]));
 
+#if defined(CONFIG_SG_LIB_LWM2M_AUTO_SEND)
+	for (size_t res_index = 0; res_index < ARRAY_SIZE(res_inst[index]); res_index++) {
+		res_inst[index][res_index].ignore = true;
+	}
+#endif
+
 	/* initialize instance resource data */
 	INIT_OBJ_RES_DATA_LEN(SECURITY_SERVER_URI_ID, res[index], i,
 			  res_inst[index], j,

subsys/net/lib/lwm2m/lwm2m_object.h
@@ -388,6 +388,13 @@ struct lwm2m_engine_res_inst {
 	uint16_t data_len;
 	uint16_t res_inst_id; /* 65535 == not "created" */
 	uint8_t  data_flags;
+#if defined(CONFIG_SG_LIB_LWM2M_AUTO_SEND)
+	uint8_t dirty: 1;
+	uint8_t force: 1;
+	uint8_t sending: 1;
+	uint8_t ignore: 1;
+	uint8_t report_after_write: 1;
+#endif
 };
 
 struct lwm2m_engine_res {
@@ -426,6 +433,13 @@ static inline void init_res_instance(struct lwm2m_engine_res_inst *ri,
 	memset(ri, 0, sizeof(*ri) * ri_len);
 	for (i = 0; i < ri_len; i++) {
 		ri[i].res_inst_id = RES_INSTANCE_NOT_CREATED;
+#if defined(CONFIG_SG_LIB_LWM2M_AUTO_SEND)
+		ri[i].dirty = false;
+		ri[i].force = IS_ENABLED(CONFIG_SG_LIB_LWM2M_AUTO_SEND_FORCE_SEND);
+		ri[i].sending = false;
+		ri[i].ignore = false;
+		ri[i].report_after_write = true;
+#endif
 	}
 }
 

subsys/net/lib/lwm2m/lwm2m_registry.c
@@ -487,10 +487,75 @@ int lwm2m_set_res_buf(const struct lwm2m_obj_path *path, void *buffer_ptr, uint1
 	res_inst->data_len = data_len;
 	res_inst->max_data_len = buffer_len;
 	res_inst->data_flags = data_flags;
+#if defined(CONFIG_SG_LIB_LWM2M_AUTO_SEND)
+	res_inst->dirty = true;
+#endif
 
 	k_mutex_unlock(&registry_lock);
 	return ret;
 }
+
+#if defined(CONFIG_SG_LIB_LWM2M_AUTO_SEND)
+int lwm2m_set_res_force_send(const struct lwm2m_obj_path *path, bool force)
+{
+	int ret;
+	struct lwm2m_engine_res_inst *res_inst = NULL;
+
+	if (path->level < LWM2M_PATH_LEVEL_RESOURCE) {
+		LOG_ERR("path must have at least 3 parts");
+		return -EINVAL;
+	}
+
+	k_mutex_lock(&registry_lock, K_FOREVER);
+	/* look up resource obj */
+	ret = path_to_objs(path, NULL, NULL, NULL, &res_inst);
+	if (ret < 0) {
+		k_mutex_unlock(&registry_lock);
+		return ret;
+	}
+
+	if (!res_inst) {
+		LOG_ERR("res instance %d not found", path->res_inst_id);
+		k_mutex_unlock(&registry_lock);
+		return -ENOENT;
+	}
+
+	res_inst->force = force;
+
+	k_mutex_unlock(&registry_lock);
+	return ret;
+}
+
+int lwm2m_set_res_report_after_write(const struct lwm2m_obj_path *path, bool report_after_write)
+{
+	int ret;
+	struct lwm2m_engine_res_inst *res_inst = NULL;
+
+	if (path->level < LWM2M_PATH_LEVEL_RESOURCE) {
+		LOG_ERR("path must have at least 3 parts");
+		return -EINVAL;
+	}
+
+	k_mutex_lock(&registry_lock, K_FOREVER);
+	/* look up resource obj */
+	ret = path_to_objs(path, NULL, NULL, NULL, &res_inst);
+	if (ret < 0) {
+		k_mutex_unlock(&registry_lock);
+		return ret;
+	}
+
+	if (!res_inst) {
+		LOG_ERR("res instance %d not found", path->res_inst_id);
+		k_mutex_unlock(&registry_lock);
+		return -ENOENT;
+	}
+
+	res_inst->report_after_write = report_after_write;
+
+	k_mutex_unlock(&registry_lock);
+	return ret;
+}
+#endif
 
 static bool lwm2m_validate_time_resource_lenghts(uint16_t resource_length, uint16_t buf_length)
 {
@@ -716,6 +781,11 @@ static int lwm2m_engine_set(const struct lwm2m_obj_path *path, const void *value
 		ret = res->post_write_cb(obj_inst->obj_inst_id, res->res_id, res_inst->res_inst_id,
 					 data_ptr, len, false, 0, 0);
 	}
+
+#if defined(CONFIG_SG_LIB_LWM2M_AUTO_SEND)
+	changed |= res_inst->force;
+	res_inst->dirty |= changed;
+#endif
 
 	if (changed && LWM2M_HAS_PERM(obj_field, LWM2M_PERM_R)) {
 		lwm2m_notify_observer_path(path);